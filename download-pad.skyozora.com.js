9e2bada65c825e530f1ed466dde67e72468e93f8
枫谷剑仙 <mapaler@163.com>
2019-09-16 02:38:24 +0800

    只能暂时先从代理来下载了

--- download-pad.skyozora.com.js
+++ download-pad.skyozora.com.js
@@ -1,5 +1,6 @@
-﻿var fs = require('fs');
-var http = require("http");
+﻿const fs = require('fs');
+const http = require("http");
+const https = require('https');
 
 function downloadPage(monArr)
 {
@@ -21,51 +22,37 @@ function downloadPage(monArr)
     }else
     {
       console.log("开始下载文件[" + mon.id + ".html]");
-      var options = { 
-        hostname: 'pad.skyozora.com', 
-        port: 80, 
-        path: '/pets/' + mon.id, 
-        method: 'get' 
+/*
+      https.get('https://pad.skyozora.com/pets/' + mon.id, function(res) {
+        console.log(res.statusCode)
+      }).on('error', console.log)
+*/
+      
+      var options = {
+        hostname: '127.0.0.1',
+        port: 8888,
+        path: 'https://pad.skyozora.com/pets/' + mon.id,
+        method: 'get'
       };
-      var req = http.request(options, function(res) { 
-        /*
-        console.log('STATUS: ' + res.statusCode); 
-        console.log('HEADERS: ' + JSON.stringify(res.headers)); 
-        res.setEncoding('utf8');
-        var contentLength = parseInt(res.headers['content-length']);
-        var downLength = 0;
-        */
+      var req = http.request(options, function(res) {
         let out = fs.createWriteStream(filename);
         res.on('data', function (chunk) { 
-          /*
-          downLength += chunk.length;
-          var progress =  Math.floor(downLength*100 / contentLength);
-          var str = "下载："+ progress +"%";
-          console.log(str);
-          */
-          out.write(chunk, function () {
-          });
+          out.write(chunk, function () {});
         });
         res.on('end', function() {
           console.log("文件[" + mon.id + ".html]下载完毕，剩余["+monArr.length+"]个");
-          /*
-          if (isNaN(contentLength)) {
-            console.log("没有 content length");
-          }else if(downLength < contentLength) {
-            console.log("下载错误，请重试[" + mon.id + ".html]");
-          }
-          */
           out.end();
           downloadPage(monArr); //下载下一个
         });
         req.on('error', function(e) { 
-          console.log('请求出现错误: ' + e.message); 
+          console.log('请求出现错误: ',e); 
         }); 
       });
       req.on('error', function(e){
-          console.log("http请求错误 [" + mon.id + ".html] 请重试");
+          console.log("http请求错误 [" + mon.id + ".html] 请重试",e);
       });
       req.end();
+      
     }
   })
 
