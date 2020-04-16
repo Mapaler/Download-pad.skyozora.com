cd59c6515443236cc3de2dbb73998a760b84c788
枫谷剑仙 <mapaler@163.com>
2020-04-16 18:45:35 +0800

    2020年4月16日

--- 下载网页数据.bat
+++ 下载网页数据.bat
@@ -2,5 +2,5 @@
 title 使用nodejs导出下载列表
 node.exe download-pad.skyozora.com.js
 title 使用Aria2下载网页
-aria2c.exe --input-file=网页下载列表.down --allow-overwrite=false --auto-file-renaming=false --remote-time=true --continue=true
+aria2c.exe --input-file=网页下载列表.down --allow-overwrite=false --auto-file-renaming=false --remote-time=true --continue=true --check-certificate=false
 pause
